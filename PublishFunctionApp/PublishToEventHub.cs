5ecab9ded1392bfcd3f029d4ede0d28f1230db36
Gary Strange <gary.strange@asos.com>
2020-04-20 11:24:14 +0100

    Add publish schema version header

--- PublishFunctionApp/PublishToEventHub.cs
+++ PublishFunctionApp/PublishToEventHub.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -8,10 +9,22 @@ namespace PublishFunctionApp
 {
     public static class PublishToEventHub
     {
+        #region Event Hub details
+        private static string eventHubNameSpace = Environment.GetEnvironmentVariable("TargetEventHubsNamespace");
+        private static readonly Uri endpointAddress = new Uri($"sb://{eventHubNameSpace}.servicebus.windows.net/");
+        private static readonly string entityPath = "gavroeh";
+        private static EventHubClient eventHubClient = EventHubClient.CreateWithManagedServiceIdentity(endpointAddress, entityPath);
+        #endregion
+
         [FunctionName("PublishToEventHub")]
         public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            SalesOrder order = SalesOrder.NewOrder();
+            EventData eventData = new EventData(order.AsJsonUTF8());
+            eventData.Properties.Add("SchemaVersion", SalesOrder.GetSchemaVersion());
+            log.LogInformation($"Sending message attempt: {order.AsJson()}");
+            eventHubClient.SendAsync(eventData);
         }
     }
 }
---
+++ PublishFunctionApp/SalesOrder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublishFunctionApp
+{
+    public class SalesOrder
+    {
+        public string OrderId = Guid.NewGuid().ToString();
+        public string AsJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+
+        public byte[] AsJsonUTF8()
+        {
+            return Encoding.UTF8.GetBytes(this.AsJson());
+        }
+
+        public static SalesOrder NewOrder()
+        {
+            return new SalesOrder();
+        }
+
+        public static string GetSchemaVersion() { return "v1.0"; }
+    }
+}
